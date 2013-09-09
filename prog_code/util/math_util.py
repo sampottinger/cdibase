30f0094fb436f6abbbfc26b3317736072f6121a5
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-09-09 16:48:53 -0600

    Updated template for parent form.

--- prog_code/util/math_util.py
+++ prog_code/util/math_util.py
@@ -49,9 +49,16 @@ def find_percentile(table_entries, target_num_words, age_months, max_words):
         return percentiles[percentile_index]
 
     upper_section_words = cur_num_words
-    lower_section_words = words_per_percentile[percentile_index+1]
+    if len(words_per_percentile) > percentile_index+1:
+        lower_section_words = words_per_percentile[percentile_index+1]
+    else:
+        lower_section_words = upper_section_words - 1
+    
     upper_section_percentile = percentiles[percentile_index]
-    lower_section_percentile = percentiles[percentile_index+1]
+    if len(percentiles) > percentile_index+1:
+        lower_section_percentile = percentiles[percentile_index+1]
+    else:
+        lower_section_percentile = upper_section_percentile - 1
 
     percentile_range = upper_section_percentile - lower_section_words
     word_range = upper_section_words - lower_section_words
