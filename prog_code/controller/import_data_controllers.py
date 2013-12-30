08200f2c79f7fa6d078028f39f57e07cda942559
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-12-30 10:35:01 -0700

    Default to last MCDI type used for import.
    The import data feature now defaults to using the last CDI type
    specified on the last upload.
    
    Resolves issue #10.

--- prog_code/controller/import_data_controllers.py
+++ prog_code/controller/import_data_controllers.py
@@ -15,13 +15,20 @@ CONFIRM_MSG = 'CSV imported into the database.'
 @app.route('/base/import_data', methods=['GET', 'POST'])
 @session_util.require_login(import_data=True)
 def import_data():
+    default_format = flask.session.get(
+        'last_format_used',
+        db_util.load_mcdi_model_listing()[0].safe_name
+    )
+
     if flask.request.method == 'GET':
         return flask.render_template(
             'import_data.html',
             cur_page='import_data',
             formats=db_util.load_mcdi_model_listing(),
+            default_format=default_format,
             **session_util.get_standard_template_values()
         )
+
     else:
         contents = io.StringIO(
             unicode(flask.request.files['file'].read()), newline=None
@@ -39,5 +46,8 @@ def import_data():
             flask.session[constants.ERROR_ATTR] = results['error']
         else:
             flask.session[constants.CONFIRMATION_ATTR] = CONFIRM_MSG
+
+        if mcdi_type != '':
+            flask.session['last_format_used'] = mcdi_type
 
         return flask.redirect('/base/import_data')
