d04b3db342fb6123666ab40c370ac371bdea8d5f
A Pottinger <samp@gleap.org>
2020-09-14 13:36:17 -0700

    interp_util_test working.

--- run_test.py
+++ run_test.py
@@ -34,7 +34,7 @@ from prog_code.util.new_csv_import_util_test import *
 from prog_code.util.db_util_test import *
 from prog_code.util.file_util_test import *
 from prog_code.util.filter_util_test import *
-#from prog_code.util.interp_util_test import *
+from prog_code.util.interp_util_test import *
 #from prog_code.util.mail_util_test import *
 #from prog_code.util.math_util_test import *
 #from prog_code.util.oper_interp_test import *
